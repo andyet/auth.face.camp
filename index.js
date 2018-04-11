3ca26e0f6e376a608dee6fde1b8cf90c8b6cff7f
Luke Karrys <lukekarrys@gmail.com>
2018-04-11 14:09:20 -0700

    auth

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const { send } = require('micro')
-const slack = require('microauth-slack')
 const dotenv = require('dotenv')
+const rp = require('request-promise')
+const { router, get } = require('microrouter')
+const qs = require('qs')
 
 dotenv.config()
 
-module.exports = slack({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackUrl: process.env.CALLBACK_URL,
-  path: '/',
-  scope: 'identity.basic'
-})((req, res, auth) => {
-  if (!auth) {
-    return send(res, 404, 'Not Found')
-  }
+const {
+  CLIENT_ID: clientId,
+  CLIENT_SECRET: clientSecret,
+  AUTH_HOST: authHost,
+  APP_URL: appUrl
+} = process.env
 
-  if (auth.err) {
-    return send(res, 403, 'Forbidden')
-  }
+const paths = {
+  token: '/token',
+  auth: '/auth'
+}
 
-  return send(res, 200)
-})
+const redirect = (res, location) => {
+  res.statusCode = 302
+  res.setHeader('Location', location)
+  res.end()
+}
+
+module.exports = router(
+  get(paths.auth, (req, res) => {
+    const params = qs.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: authHost + paths.token,
+      scope: 'chat:write:user channels:read'
+    })
+    redirect(res, `https://slack.com/oauth/authorize?${params}`)
+  }),
+  get(paths.token, async (req, res) => {
+    const { error, code } = req.query
+
+    if (error) {
+      return redirect(res, `${appUrl}?${qs.stringify({ error })}`)
+    }
+
+    const access = await rp({
+      uri: 'https://slack.com/api/oauth.access',
+      json: true,
+      qs: {
+        code: req.query.code,
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: authHost + paths.token
+      }
+    })
+
+    redirect(res, `${appUrl}?${qs.stringify(access)}`)
+  })
+)
