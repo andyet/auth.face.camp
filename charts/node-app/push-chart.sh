6f8b71fc99b5d0ed087107aae9a12c95e32aba25
Heather Young <heather@andyet.net>
2019-05-17 13:03:43 -0700

    recreate pods on deploy

--- charts/node-app/push-chart.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-helm package ~/Desktop/charts/node-app -d ~/charts &&\
-helm repo index ~/charts --url https://andyet-helm-charts.storage.googleapis.com &&\
-~/Desktop/sync-repo.sh ~/charts andyet-helm-charts
